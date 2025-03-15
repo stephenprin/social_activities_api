7161f78f73b9c1e77dc4aa71503b30659ecc88cf
Prince Nmezi <stephenprince427@gmail.com>
2025-03-15 12:30:00 +0100

    folder structure and mapping out domain

---
+++ Domain/Activity.cs
@@ -0,0 +1,16 @@
+namespace Domain;
+
+public class Activity
+{
+    public string Id{ get; set; } = Guid.NewGuid().ToString();
+    public required string Title { get; set; }
+    public DateTime Date { get; set; }
+    public bool IsCancelled { get; set; }
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+
+}
